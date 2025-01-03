21cddd0ef0da16b38dc12fec0a3f50463e5cb823
mugni1 <asepabdulmugni22@gmail.com>
2025-01-03 17:27:58 +0700

    add version text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,11 +7,13 @@ Header.propTypes = {
 export function Header({ className }) {
   return (
     <section
-      className={`w-full flex px-5 py-2 shadow-md sticky top-0 ${className} `}
+      className={`w-full justify-between items-center text-white flex px-5 py-2 shadow-md sticky top-0 ${className} `}
     >
       <Link to="/">
         <img src="/left-arrow-alt-svgrepo-com.png" alt="" className="h-8 w-8" />
       </Link>
+
+      <span>v1.0</span>
     </section>
   );
 }
